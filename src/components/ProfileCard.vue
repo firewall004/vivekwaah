6509ad70e200ee004ea6808e4cc0648629548eb9
Vivek Kumar <kumar97vivek@gmail.com>
2023-07-22 03:20:23 +0530

    Fix email

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -29,7 +29,7 @@
           >
             <a
               type="button"
-			  href="mailto:{{ email }}"
+			  :href="'mailto:' + profile.email"
 			  target="_blank"
               class="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             >
