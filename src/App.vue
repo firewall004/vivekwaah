0ca3a92904dda4ac5f820a5d91f550915ea2e346
Vivek Kumar <kumar97vivek@gmail.com>
2023-07-23 02:32:49 +0530

    Fix page height

--- src/App.vue
+++ src/App.vue
@@ -39,29 +39,10 @@
 </template>
 
 <script setup>
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Popover,
-  PopoverButton,
-  PopoverOverlay,
-  PopoverPanel,
-  TransitionChild,
-  TransitionRoot,
-} from "@headlessui/vue";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
-import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
+
 import ProfileCard from "./components/ProfileCard.vue";
 import AppFooter from "./components/common/AppFooter.vue";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 </script>
 
 <style scoped>
